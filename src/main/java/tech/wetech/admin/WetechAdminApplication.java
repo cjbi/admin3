6951fcb93ea2b82bd63f86a5c799ddb6d2c3813b
Jinbao Cheng <cjbi@outlook.com>
2018-05-14 14:48:52 +0800

    更新代码

--- src/main/java/tech/wetech/admin/WetechAdminApplication.java
+++ src/main/java/tech/wetech/admin/WetechAdminApplication.java
@@ -4,15 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import tk.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.quartz.QuartzAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {QuartzAutoConfiguration.class})
 public class WetechAdminApplication implements CommandLineRunner {
 
     private Logger logger = LoggerFactory.getLogger(WetechAdminApplication.class);
