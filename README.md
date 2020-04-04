7071f4b5dbbe0f00859795024324d18083619caf
Jinbao Cheng <cjbi@outlook.com>
2020-04-04 21:24:18 +0800

    更新文档

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Wetech-Admin
 
-Wetech-Admin 是基于 Spring Boot 2.0 + Mybatis 的轻量级后台管理系统，适用于中小型项目的管理后台，支持按钮级别的权限控制，系统具有最基本的用户管理、角色管理、资源管理、组织机构管理、系统日志、代码生成器等通用性功能，企业或个人可直接在此基础上进行开发，扩展，添加各自的需求和业务功能！http://wetech.tech/wetech-admin/
+Wetech-Admin 是基于 Spring Boot 2.0 + Mybatis 的轻量级后台管理系统，适用于中小型项目的管理后台，支持按钮级别的权限控制，系统具有最基本的用户管理、角色管理、权限管理等通用性功能，企业或个人可直接在此基础上进行开发，扩展，添加各自的需求和业务功能！http://wetech.tech/wetech-admin/
 
 
 ## 技术选型
@@ -11,14 +11,10 @@ Wetech-Admin 是基于 Spring Boot 2.0 + Mybatis 的轻量级后台管理系统�
 ----|------|----|----
 Spring Boot | 应用框架 | 2.0.0.RELEASE | [https://projects.spring.io/spring-boot/](https://projects.spring.io/spring-boot/)
 MyBatis | ORM框架 | 3.2.1 |  [http://www.mybatis.org/mybatis-3/zh/index.html](http://www.mybatis.org/mybatis-3/zh/index.html)
-Mapper | MyBatis 通用 Mapper4 | 4.0.0 |  [https://gitee.com/free/Mapper](https://gitee.com/free/Mapper)
-PageHelper | MyBatis 分页插件 | 5.1.2 |  [https://gitee.com/free/Mybatis_PageHelper](https://gitee.com/free/Mybatis_PageHelper)
+MyBatis-Ext | MyBatis 通用 Mapper4 | 4.0.0 |  [https://github.com/cjbi/mybatis-ext](https://gitee.com/free/Mapper)
 Maven | 项目构建管理 | 4.0.0 |  [http://maven.apache.org](http://maven.apache.org/)
-MyBatis Generator | 代码生成 | 1.3.5 |  [http://www.mybatis.org/generator/index.html](http://www.mybatis.org/generator/index.html)
-Thymeleaf | 模板引擎 | 3.0.9.RELEASE |  [https://www.thymeleaf.org/](https://www.thymeleaf.org/)
 Apache Shiro | 安全框架 | 1.2.2 |  [http://shiro.apache.org](http://www.mybatis.org/generator/index.html)
 Logback | 日志组件 | 1.1.3 |  [https://logback.qos.ch](https://logback.qos.ch/)
-Druid | 数据库连接池 | 0.2.23 |  [https://github.com/alibaba/druid](https://github.com/alibaba/druid)
 Hibernate Validator | 后端校验框架 | 5.4.2.Final | [http://hibernate.org/validator/](http://hibernate.org/validator/)
 swagger2 | 在线文档生成 |  2.9.2 | [https://swagger.io/](https://swagger.io/) 
 
@@ -26,11 +22,8 @@ swagger2 | 在线文档生成 |  2.9.2 | [https://swagger.io/](https://swagger.i
 
 技术 | 名称 | 版本 |  官网
 ----|------|----|----
-jQuery | 优秀的Javascript库 | 3.2.1 |  [http://jquery.com/](http://jquery.com/)
-Admin-LTE| 基于Bootstrap的前端框架 | 3.0.0 |  [https://adminlte.io/](https://adminlte.io/)
-Bootstrap Table | 数据表格 | 1.10.16 |  [http://bootstrap-table.wenzhixin.net.cn/zh-cn/getting-started/](http://bootstrap-table.wenzhixin.net.cn/zh-cn/getting-started/)
-Layer | jQuery弹出层插件 | 3.1.0 |  [http://layer.layui.com/](http://layer.layui.com/)
-ZTree | jQuery树插件 | 3.5.29 |  [http://www.treejs.cn](http://www.treejs.cn)
+Vue | 渐进式JavaScript 框架 | 3.2.1 |  [https://cn.vuejs.org/](https://cn.vuejs.org/)
+ANTD PRO VUE| 基于 Ant Design Vue 的 Pro 第三方实现 | 3.0.0 |  [https://pro.loacg.com/](https://pro.loacg.com/)
 
 ## 软件需求
 
@@ -48,7 +41,10 @@ ZTree | jQuery树插件 | 3.5.29 |  [http://www.treejs.cn](http://www.treejs.cn)
 
 
 ## 预览图
-
+![](doc/image/1.JPG)
+![](doc/image/2.JPG)
+![](doc/image/3.JPG)
+![](doc/image/4.JPG)
 
 ## 在线访问
 
--- doc/sql/schema.sql
+++ doc/sql/schema.sql
@@ -1,53 +1,35 @@
-create schema wetech_admin collate utf8_general_ci;
+create schema wetech_admin collate utf8mb4_unicode_ci;
 
 create table sys_log
 (
     id          bigint auto_increment comment '主键'
         primary key,
-    username    varchar(100) null comment '用户名',
-    ip          varchar(45) null comment '用户ip',
-    req_method  varchar(200) null comment '请求方法',
-    req_uri     text null comment '请求URL',
-    exec_method varchar(200) null comment '执行方法',
-    exec_time   bigint null comment '响应时间',
-    args        text null comment '参数',
-    return_val  text null comment '返回值',
-    exec_desc   varchar(200) null comment '描述',
-    status      varchar(45) null comment '状态',
+    username    varchar(100)                       null comment '用户名',
+    ip          varchar(45)                        null comment '用户ip',
+    req_method  varchar(200)                       null comment '请求方法',
+    req_uri     text                               null comment '请求URL',
+    exec_method varchar(200)                       null comment '执行方法',
+    exec_time   bigint                             null comment '响应时间',
+    args        text                               null comment '参数',
+    return_val  text                               null comment '返回值',
+    exec_desc   varchar(200)                       null comment '描述',
+    status      varchar(45)                        null comment '状态',
     create_time datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '创建时间'
 ) comment '系统日志表';
-
-create table sys_organization
-(
-    id         bigint auto_increment comment '编号'
-        primary key,
-    name       varchar(100) null comment '组织名称',
-    parent_id  bigint null comment '父编号',
-    parent_ids varchar(100) null comment '父编号列表',
-    available  tinyint(1) default 0 null comment '是否有效',
-    priority   int null comment '优先级',
-    leaf       tinyint(1) default 0 null comment '叶子节点'
-) comment '组织机构表';
-
-create index idx_sys_organization_parent_id
-    on sys_organization (parent_id);
-
-create index idx_sys_organization_parent_ids
-    on sys_organization (parent_ids);
 
 create table sys_permission
 (
     id         bigint auto_increment comment '编号'
         primary key,
-    name       varchar(100) null comment '名称',
-    type       tinyint(1) not null comment '资源类型(1.菜单 2.按钮或文本块)',
-    parent_id  bigint null comment '父编号',
-    parent_ids varchar(100) null comment '父编号列表',
-    permission varchar(100) null comment '权限字符串',
-    icon       varchar(100) null comment '图标',
-    sort       int default 0 not null comment '排序',
+    name       varchar(100)         null comment '名称',
+    type       tinyint(1)           not null comment '资源类型(1.菜单 2.按钮或文本块)',
+    parent_id  bigint               null comment '父编号',
+    parent_ids varchar(100)         null comment '父编号列表',
+    permission varchar(100)         null comment '权限字符串',
+    icon       varchar(100)         null comment '图标',
+    sort       int        default 0 not null comment '排序',
     status     tinyint(1) default 1 null comment '是否有效',
-    config     json null comment '权限配置'
+    config     json                 null comment '权限配置'
 ) comment '资源表';
 
 create index idx_sys_resource_parent_id
@@ -60,11 +42,11 @@ create table sys_role
 (
     id             bigint auto_increment comment '编号'
         primary key,
-    role           varchar(100) not null comment '唯一标识',
-    name           varchar(100) null comment '角色名称',
-    description    varchar(100) null comment '描述',
+    role           varchar(100)         not null comment '唯一标识',
+    name           varchar(100)         null comment '角色名称',
+    description    varchar(100)         null comment '描述',
     status         tinyint(1) default 1 null comment '状态 1.正常 0.禁用',
-    permission_ids varchar(500) null comment '资源编号列表'
+    permission_ids varchar(500)         null comment '资源编号列表'
 ) comment '角色表';
 
 create index idx_sys_role_resource_ids
@@ -74,10 +56,10 @@ create table sys_user
 (
     id       bigint auto_increment comment '编号'
         primary key,
-    username varchar(100) null comment '用户名',
-    password varchar(100) null comment '密码',
-    salt     varchar(100) null comment '盐值',
-    role_ids varchar(100) null comment '角色列表',
+    username varchar(100)         null comment '用户名',
+    password varchar(100)         null comment '密码',
+    salt     varchar(100)         null comment '盐值',
+    role_ids varchar(100)         null comment '角色列表',
     locked   tinyint(1) default 0 null comment '是否锁定',
     constraint idx_sys_user_username
         unique (username)
