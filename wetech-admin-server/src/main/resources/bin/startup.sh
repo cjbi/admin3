adf0154e4d776230b7bbbb2d48b38cc525a775ee
cjbi <cjbi@outlook.com>
2020-04-16 17:54:20 +0800

    修改脚本

--- wetech-admin-server/src/main/resources/bin/startup.sh
+++ wetech-admin-server/src/main/resources/bin/startup.sh
@@ -7,7 +7,7 @@ env=@spring.application.proflies@
 jar_name=@build.finalName@.jar
 
 # 根据启动的jar包名称关闭旧的进程实例
-pid=`ps -ef | grep ${jar_name} | grep -v grep | awk "{print $2}"`
+pid=`ps -ef | grep ${jar_name} | grep -v grep | awk '{print $2}'`
 echo "Wetech-Admin服务旧应用进程id：$pid"
 if [ -n "$pid" ]
 then
