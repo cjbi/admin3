11408f303f2b2ccab2ea9365547a281c83b2d15e
Cheng Jinbao <cjbi@outlook.com>
2023-02-13 20:56:25 +0800

    feat: 增加sql初始化数据

--- admin3-server/src/main/resources/data.sql
+++ admin3-server/src/main/resources/data.sql
@@ -163,6 +163,9 @@ INSERT INTO resource (id, icon, name, parent_ids, permission, type, url, parent_
 INSERT INTO resource (id, icon, name, parent_ids, permission, type, url, parent_id) VALUES (19, null, '新增组织架构', null, 'organization:create', 1, null, 4);
 INSERT INTO resource (id, icon, name, parent_ids, permission, type, url, parent_id) VALUES (20, null, '修改组织架构', null, 'organization:update', 1, null, 4);
 INSERT INTO resource (id, icon, name, parent_ids, permission, type, url, parent_id) VALUES (21, null, '删除组织架构', null, 'organization:delete', 1, null, 4);
+INSERT INTO resource (id, icon, name, parent_ids, permission, type, url, parent_id) VALUES (22, 'Timer', '操作日志', null, 'log:view', 0, '/logs', 3);
+INSERT INTO resource (id, icon, name, parent_ids, permission, type, url, parent_id) VALUES (23, null, '清空日志', null, 'log:clean', 1, null, 23);
+INSERT INTO resource (id, icon, name, parent_ids, permission, type, url, parent_id) VALUES (24, null, '查看日志', null, 'log:view', 1, null, 23);
 
 INSERT INTO role_resource (role_id, resource_id) VALUES (1, 2);
 INSERT INTO role_resource (role_id, resource_id) VALUES (1, 3);
@@ -184,6 +187,9 @@ INSERT INTO role_resource (role_id, resource_id) VALUES (1, 18);
 INSERT INTO role_resource (role_id, resource_id) VALUES (1, 19);
 INSERT INTO role_resource (role_id, resource_id) VALUES (1, 20);
 INSERT INTO role_resource (role_id, resource_id) VALUES (1, 21);
+INSERT INTO role_resource (role_id, resource_id) VALUES (1, 22);
+INSERT INTO role_resource (role_id, resource_id) VALUES (1, 23);
+INSERT INTO role_resource (role_id, resource_id) VALUES (1, 24);
 INSERT INTO role_resource (role_id, resource_id) VALUES (2, 2);
 INSERT INTO role_resource (role_id, resource_id) VALUES (2, 3);
 INSERT INTO role_resource (role_id, resource_id) VALUES (2, 6);
