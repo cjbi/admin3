d29cb98b62c59aa824c77362344a4b399d834288
Cheng Jinbao <cjbi@outlook.com>
2023-02-13 21:08:12 +0800

    feat: 优化前端代码

--- admin3-server/src/test/java/tech/wetech/admin3/controller/LogControllerTest.java
+++ admin3-server/src/test/java/tech/wetech/admin3/controller/LogControllerTest.java
@@ -1,7 +1,9 @@
 package tech.wetech.admin3.controller;
 
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,6 +21,7 @@ import static tech.wetech.admin3.Constants.TOKEN_HEADER_NAME;
  * @author cjbi
  */
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @AutoConfigureMockMvc
 public class LogControllerTest {
 
