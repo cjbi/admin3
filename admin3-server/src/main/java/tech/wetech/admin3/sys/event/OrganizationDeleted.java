96240731382d76e1fcb637fcb972caa68954f9bc
Cheng Jinbao <cjbi@outlook.com>
2023-02-15 11:47:53 +0800

    hotfix: optimize events

---
+++ admin3-server/src/main/java/tech/wetech/admin3/sys/event/OrganizationDeleted.java
@@ -0,0 +1,10 @@
+package tech.wetech.admin3.sys.event;
+
+import tech.wetech.admin3.common.DomainEvent;
+import tech.wetech.admin3.sys.model.Organization;
+
+/**
+ * @author cjbi
+ */
+public record OrganizationDeleted(Organization organization) implements DomainEvent {
+}
